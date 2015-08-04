3d8912ad92f87c4c84bb2affbc80c9a2a02cb1c1
LandoB <jrolandobernal@gmail.com>
2015-08-04 08:26:15 -0500

    Added routing. Now the NavBar works fine. Still need to work on the controllers

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var app = angular.module('myApp', ['ngRoute', 'firebase']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    .when("/", {templateUrl: 'partials/home.html', controller: 'HomeCtrl'})
+    .when('/about', {templateUrl: 'partials/about.html', controller: 'AboutCtrl'})
+    .when('/contact', {templateUrl:'partials/contact.html', controller:'ContactCtrl'})
+    .when('/gallery', {templateUrl:'partials/gallery.html', controller:'GalleryCtrl'})
+    .when('/home', {templateUrl:'partials/home.html', controller:'HomeCtrl'})
+    .when('/sponsors', {templateUrl:'partials/sponsors.html', controller:'SponsorsCtrl'})
+    .when('/teams', {templateUrl:'partials/teams.html', controller:'TeamsCtrl'})
+    .when('/tryouts', {templateUrl:'partials/tryouts.html', controller:'TryoutCtrl'})
+    .otherwise('/404', {templateUrl: 'partials/404.html', controller: 'ErrCtrl'});
+}]);
+
+
+app.constant('API_URL', 'https://nash-int-futbol-club.firebaseapp.com');
+
