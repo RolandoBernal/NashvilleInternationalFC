5dc33adb3db537c1faab08e62ba8d6204cca0225
Hannah Smithmier <smithmier.hannah@gmail.com>
2015-08-31 16:55:27 -0500

    hiding and showing

--- js/myscripts/tryoutjq.js
+++ js/myscripts/tryoutjq.js
@@ -2,10 +2,11 @@
 
  $('#form_id').hide(); //Initially form wil be hidden.
 
-  $('#button_id').click(function() {
-   $('#form_id').show();//Form shows on button click
+  $('#button_id').on('click', function(e) {
+   $('#form_id').toggle();//Form shows on button click
+		$(this).toggleClass('form_id');
    });
-  $('#submit_id').click(function(){
-  	$('#form_id').hide();
-  })
+  // $('#submit_id').click(function(){
+  // 	$('#form_id').hide();
+  // })
  });
